9a5cc7a5dd9367f2d790cbfa11b45bedf7ea11f7
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-03-11 11:39:29 +0000

    Glance now works and importing an image functions

--- templates/credentials.sh
+++ templates/credentials.sh
@@ -4,3 +4,4 @@ export OS_TENANT_NAME=openstackDemo
 export OS_USERNAME=adminUser
 export OS_PASSWORD=secretword
 export OS_AUTH_URL="http://{{ keystone_server }}:5000/v2.0/"
+export SERVICE_ENDPOINT=http://{{ keystone_server }}:35357/v2.0
